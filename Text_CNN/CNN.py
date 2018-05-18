509e1aa0edbb89486c3f6890351dc401a804f456
qianyuan <qianyuan@cdsb.com>
2018-05-18 17:42:12 +0800

    添加cnn文本分类算法

--- Text_CNN/CNN.py
+++ Text_CNN/CNN.py
@@ -8,8 +8,8 @@ class TextCNN():
     def __init__(self, vector_length,lable_length):
         self.vector_length=vector_length
         self.lable_length=lable_length
-        self.input_x=tf.placeholder(shape=[None,self.vector_length],name='input_x',dtype=tf.int32)
-        self.input_y=tf.placeholder(shape=[None,self.lable_length],name='input_y',dtype=tf.int32)
+        self.input_x=tf.placeholder(shape=[None,self.vector_length],name='input_x',dtype=tf.float32)
+        self.input_y=tf.placeholder(shape=[None,self.lable_length],name='input_y',dtype=tf.float32)
     
     
     def weight(self,shape):
@@ -21,9 +21,13 @@ class TextCNN():
         biases=tf.Variable(initial_value=tf.random_normal(shape=shape,stddev=1.0),name='biases')
         return biases
     
+    def filters(self,shape):
+        filters=tf.Variable(initial_value=tf.random_normal(shape,dtype=tf.float32),name='filter')
+        return filters
+        
     def create_model(self):
         with tf.name_scope('conv'):
-            conv=tf.layers.conv1d(self.input_x,)
+            conv=tf.nn.conv2d(self.input_x,filter=self.filters([]))
         
     
     
