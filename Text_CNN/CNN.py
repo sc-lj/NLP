4de77bebb03a535d56f0f479dedf193b3e9fb8e5
qianyuan <qianyuan@cdsb.com>
2018-05-18 16:45:01 +0800

    添加cnn文本分类算法

--- Text_CNN/CNN.py
+++ Text_CNN/CNN.py
@@ -5,13 +5,25 @@ import tensorflow as tf
 
 
 class TextCNN():
-    def __init__(self, vertor_length,lable_length):
-        self.input_x=tf.placeholder(shape=[None,vertor_length],name='input_x',dtype=tf.int32)
-        self.input_y=tf.placeholder(shape=[None,lable_length],name='input_y',dtype=tf.int32)
+    def __init__(self, vector_length,lable_length):
+        self.vector_length=vector_length
+        self.lable_length=lable_length
+        self.input_x=tf.placeholder(shape=[None,self.vector_length],name='input_x',dtype=tf.int32)
+        self.input_y=tf.placeholder(shape=[None,self.lable_length],name='input_y',dtype=tf.int32)
     
     
-    def weight(self):
-        weight=tf.Variable(validate_shape=[],initial_value=tf.random_normal_initializer())
+    def weight(self,shape):
+        weight=tf.Variable(initial_value=tf.random_normal(shape,stddev=2.0),name='weight')
+        
+        return weight
+    
+    def biases(self,shape):
+        biases=tf.Variable(initial_value=tf.random_normal(shape=shape,stddev=1.0),name='biases')
+        return biases
+    
+    def create_model(self):
+        with tf.name_scope('conv'):
+            conv=tf.layers.conv1d(self.input_x,)
         
     
     
