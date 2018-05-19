0cc3491afd58774bc91ed0c14f66d335f819ff04
lj <779365135@qq.com>
2018-05-19 11:45:11 +0800

    cnn 文本分类算法

--- Text_CNN/CNN.py
+++ Text_CNN/CNN.py
@@ -10,7 +10,7 @@ class TextCNN():
         :param vector_length: 词向量的长度
         :param lable_length: 类别长度
         """
-        self.filter_size=CNN.filter_size
+        self.filter_sizes=seq.filter_sizes
         self.vector_length=vector_length
         self.lable_length=lable_length
         self.input_x=tf.placeholder(shape=[None,self.vector_length],name='input_x',dtype=tf.float32)
@@ -32,10 +32,20 @@ class TextCNN():
     
     def create_model(self):
         with tf.name_scope('conv'):
-            # 由于conv2d卷积shape为[height, width, in_channels, out_channels]，
-            # in_channels: 图片的深度；在文本处理中深度为1，需要添加的一个1，增加其维度。
-            filtersize=[self.filter_size,self.vector_length,1,CNN.filter_nums]
-            convd=tf.nn.conv2d(self.input_x,filter=self.weight(filtersize),strides=(1,1,1,1),padding='VALID',name='convd')
+            pools=[]
+            for i,filter_size in enumerate(self.filter_sizes):
+                # 由于conv2d卷积shape为[height, width, in_channels, out_channels]，
+                # in_channels: 图片的深度；在文本处理中深度为1，需要添加的一个1，增加其维度。
+                filtersize=[filter_size,self.vector_length,1,seq.filter_nums]
+                convd=tf.nn.conv2d(self.input_x,filter=self.weight(filtersize),strides=(1,1,1,1),padding='VALID',name='convd')
+                biase_shape=[seq.filter_nums]
+                convd=tf.nn.relu(tf.nn.bias_add(convd,self.biases(biase_shape)),name='relu')
+                pooled=tf.nn.max_pool(convd,ksize=(1,self.vector_length-filter_size+1,1,1),strides=(1,1,1,1),padding='SAME',name='pool')
+                pools.append(pooled)
 
-        with tf.name_scope('pool'):
-            max_pool=tf.nn.max_pool(convd,ksize=())
+            num_filters_total=seq.filter_nums*len(self.filter_sizes)
+            self.h_pool=tf.concat(pools,3)
+            self.pool_flat=tf.reshape(self.h_pool,[-1,num_filters_total])
+
+
+
--- Text_CNN/config.py
+++ Text_CNN/config.py
@@ -1,8 +1,8 @@
 # coding:utf-8
 
-class CNN():
+class seq():
     # 卷积的宽度，在cnn中，卷积的长度为词向量的长度，宽度表示包含多少词
-    filter_size=2
+    filter_sizes=[2,4,6]
     
     # 每种卷积的个数
     filter_nums=2
