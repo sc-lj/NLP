e8048c8f7f0d3a0dd288d79978a62d269a508385
lujun <779365135@qq.com>
2018-05-31 11:34:18 +0800

    CLSTM 文本分类

--- Classify/CLSTM/Config.py
+++ Classify/CLSTM/Config.py
@@ -14,7 +14,7 @@ def argument():
     # cnn模型卷积核大小，给定的list，是多个卷积核的大小
     cnn_parse.add_argument('--cnn_filter_size',default=[3,4,5],type=list,help='Comma-separated filter sizes (default: "3,4,5")')
     # 输入cnn模型的句子最大长度
-    cnn_parse.add_argument('--cnn_seq_length',default=500,type=int,help='max sequence length of every sequence')
+    cnn_parse.add_argument('--cnn_maxlen',default=300,type=int,help='max sequence length of every sequence')
     # cnn模型的学习率
     cnn_parse.add_argument('--cnn_learn_rate',default=0.98,type=int,help='learning rate of cnn model')
     #
--- Classify/CLSTM/Model.py
+++ Classify/CLSTM/Model.py
@@ -1,4 +1,32 @@
 # coding:utf-8
 
-import tensorflow as tf
+import os,sys
+sys.path.append(os.path.dirname(__file__))
 
+import tensorflow as tf
+from Config import *
+
+class CNN():
+    def __init__(self):
+        self.arg=argument()
+        self.input=tf.placeholder(shape=[None,self.arg.maxlen],name='input',dtype=tf.float32)
+        self.label=tf.placeholder(shape=[None,],name='label',dtype=tf.float32)
+
+    def embedding_variable(self):
+        embed=tf.Variable(tf.random_uniform(shape=[],minval=-0.25,maxval=0.25),name='embedding')
+        return tf.nn.embedding_lookup(embed,self.input)
+
+    def weight_variable(self,shape):
+        weight=tf.Variable(tf.truncated_normal(shape=shape,mean=0,stddev=0.5,name='weight'))
+        return weight
+
+    def bias_variable(self,shape):
+        bias=tf.Variable(tf.constant(0,dtype=tf.float32,shape=shape),name='bias')
+        return bias
+
+    def create_model(self):
+
+        with tf.name_scope('conv'):
+            filter_size=()
+            conv=tf.nn.conv2d(self.embedding_variable(),filter=())
+        pass
