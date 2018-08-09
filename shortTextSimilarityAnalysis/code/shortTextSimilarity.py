5d6a931cb3392ef23d1c4748ee513812973b7bc9
lujun <779365135@qq.com>
2018-08-09 18:07:21 +0800

    短文本相似性计算

--- shortTextSimilarityAnalysis/code/shortTextSimilarity.py
+++ shortTextSimilarityAnalysis/code/shortTextSimilarity.py
@@ -6,55 +6,69 @@
 
 import os,re,jieba
 import numpy as np
-
-def stopwords():
-    """
-    读取停用词
-    :return:
-    """
-    with open('../../Dataset/stopwords/stopwords.txt','r') as f:
-        stopword=f.read()
-    return stopword
-
-def cutWords(content):
-    """
-    分句子后，然后分词
-    :param content:
-    :return:
-    """
-    content=content.lower()
-    stopword = stopwords()
-    splitdot=re.compile('[。，？！：,.、;]')# 断句的标点呼号
-    sentences=splitdot.split(content)# 切分成多个语句
-    words=[]
-    for sentence in sentences:
-        rule = re.compile(r"[^a-zA-Z\u4e00-\u9fa5]")  # 只留字母、中文。
-        sentence=rule.sub('',sentence)
-        word=jieba.cut(sentence)#结巴对句子进行分词
-        words.extend(list(word))
-
-    # 过滤停用词
-    filterstopword=list(filter(lambda x:x if x not in stopword else None, words))
-    return filterstopword
+from TextProcess import *
 
 
-def sentenceSimilarity(sentence1,sentence2):
+def semanticSimilarity(sentence1,sentence2):
     """
-    计算语句相似度
+    计算语句的语义相似度
     :param sentence1:
     :param sentence2:
     :return:
     """
+    sentence1=cutWords(sentence1)
+    sentence2=cutWords(sentence2)
+    sentenset=list(set(sentence1).union(set(sentence2)))
+    sentence1vector=[]
+    sentence2vector=[]
+
+
+# 返回格式{code: '文字字符串', }
+def cilin():
+    print('------当前进行《同义词词林》的读入操作------')
+    cilinFilePath = '../script/synonym_ex.txt'
+    cilinFileObject = open(cilinFilePath, 'r')  # 进行分词文件的读取
+    cilinDatas = {}
+    for line in cilinFileObject:
+        word = line.strip('\n')  # 去除换行符
+        cilinDatas[word[0:8]] = word[9:].split(" ")
+    return cilinDatas
+
+cilinDatas=cilin()
+def wordEncoding(word,cilinDatas=cilinDatas):
+    """
+    从同义词林中找到词的编码
+    :param word:
+    :param cilinDatas:
+    :return:
+    """
+    wordEncodingDatas=[]
+    for code, words in cilinDatas.items():
+        if word in words:
+            wordEncodingDatas.append(code)
+    return wordEncodingDatas
 
 
 
 def wordSimilarity(word1,word2):
     """
-    计算词语相似度,该方法是基于
+    计算词语相似度,该方法是基于哈工大社会计算与信息检索研究中心同义词词林扩展版计算两个词的相似度
     :param word1:
     :param word2:
     :return:
     """
+    word1Encode=wordEncoding(word1)
+    word2Encode=wordEncoding(word2)
+    if len(word1Encode)==0 or len(word2Encode)==0:
+        return 0
+
+    for code1 in word1Encode:
+        for code2 in word2Encode:
+            pass
+
+
+
+
 
 
 def wordOrderSimilarity(order1,order2):
@@ -64,6 +78,7 @@ def wordOrderSimilarity(order1,order2):
     :param order2:
     :return:
     """
+    assert len(order1)==len(order2),u"两个句子的词序长度不相等"
     order1=np.array(order1)
     order2=np.array(order2)
     #词序向量差的二范数
@@ -71,15 +86,13 @@ def wordOrderSimilarity(order1,order2):
     #词序向量和二范数
     orderSum = np.linalg.norm(order1 + order2, ord=2)
     Sr=1-np.float32(orderSubtract)/np.float32(orderSum)
-    # Sr=Sr/len(order1)
     return Sr
 
 
 if __name__ == '__main__':
     order1= [1,2,3,4,5,6,7,8,9]
     order2= [1,2,3,9,5,6,7,8,4]
-    sr=wordOrderSimilarity(order1,order2)
-    print(sr)
-
-
-
+    # sr=wordOrderSimilarity(order1,order2)
+    a={"1": '机器学习是一门多领域交叉学科', "2": '涉及概率论、统计学、逼近论、凸分析、算法复杂度理论等多门学科'}
+    # semanticSimilarity(a['1'],a['2'])
+    cilin()
