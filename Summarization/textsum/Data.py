ca9c2b3167a81b0112c398a31355043259473e92
lujun <779365135@qq.com>
2018-09-18 18:11:34 +0800

    文本摘要

--- Summarization/textsum/Data.py
+++ Summarization/textsum/Data.py
@@ -1,18 +1,4 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
+# coding:utf-8
 """Data batchers for data described in ..//data_prep/README.md."""
 
 import glob,jieba
@@ -122,6 +108,7 @@ def extract_html(content):
     #  将双分号里面的句子不进行分割。
     Dquotes=['"','“','”']
     for paragraph in annotated_text:
+        print(paragraph)
         sentences=""
         for text, annotations in paragraph:
             sentences+=text
@@ -276,6 +263,6 @@ def ToSentences(paragraph, include_token=True):
 
 if __name__ == '__main__':
 
-    content=""
+    content=u"图为第三季港澳台学子金华活动闭幕式合影　　吴俊鹏摄来自港澳台地区的37名高校学子近日来到黄大仙故里浙江金华，参加第三季“台港澳学子走进‘侨仙’故里金华”活动，开启了为期10天的文化寻根之旅。回家的感觉真好“关上房门是星级酒店、打开房门是温馨家庭、走出家门是乡野生活”，这是金华首创的“家+”模式。在前两季“台港澳学子走进金华古村落”活动中，它深受学子欢迎。学子们入住“家+”有一定的仪式。“家+”的住家们手拿对联站在村口，学子们则需要在另一边的对联中，找出与自己入住之处相呼应的对联。“少小离家老大回，下一句是？”“乡音无改鬓毛衰。”“哈哈，孩子，欢迎你到我家。”配对成功后，学生即可跟着住家回去。来自澳门、目前在中国人民大学读研的吴海琴对此感触特别深：“这里的一山一水、一家一户、每个人一下子变得非常亲切，好像前世注定的亲人一样，注定在某个地方相遇，真的太美！”每当夜幕降临，炊烟升起，在古村落的一幢幢房子里，一户户“家+”灯火通明，港澳台学子与住家叔叔阿姨们一起用餐，一道聊天，一片欢声笑语。金华市港澳办主任章宏说，金华要通过“家+住宿”“家+餐饮”“家+活动”，特别是“家+特殊人文”，打造环境古朴、设施现代、人文诚善，既有住家体验又有旅行情怀，还有研学修行的台港澳学子的“情感驿站”和“心灵港湾”。文化传承的使者作为中华优秀传统文化的一种精华浓缩，黄大仙文化已经在22个国家和地区传播，金华传统村落文化也留在了台港澳与海外高校学子的记忆里。“我们两岸三地青年一代有责任当好保护和传承中华传统文化的使者。”来自香港大学的丁泽棋认为，金华要与粤港澳联手，更好地保护和发扬“黄大仙文化”，使其成为东西方文化兼容的典型代表。澳门城市大学万淑娟同学则建议，金华要用好作为黄大仙文化发源地、港澳影视演员常年在横店拍摄作品的独特优势，精心制作黄大仙文化和传统村落文化影视作品及动漫产品，延伸文化产业链，使自身成为与“一带一路”沿线国家对接、凝聚海内外华人的重要纽带。活动组织者之一、北大金华选调生吴俊鹏说，每一季港澳台学子进古村活动中，都会开展以传统文化的传承、融合、发展为主线的海峡两岸暨港澳青年论坛，以此推进两岸暨港澳青年主动融入国家发展大局，共绘中国梦。乡梓故里的依恋“黄大仙在港澳地区可谓家喻户晓，寄托了我们祖祖辈辈对中华传统文化的归属与依恋。没想到黄大仙来自金华，而且有1600多年历史了。”第一次来金华文化寻根的香港大学学生蓝晓珺感慨不已。金华是黄大仙的祖庭所在地。晋代黄大仙因擅长炼丹和医术，一生劝善扬善、惩恶除奸、有求必应，泽被一方，民众遂为之修祠，世代祀奉。随着金华乡亲旅居港澳地区和海外，黄大仙信仰也走向港澳和海外，并成为众多华侨华人心中的“侨仙”。金华一直是港澳台地区和海外信众的朝圣地，两岸三地经常在这里联合举办庙会和旅游节。"
     extract_html(content)
 
