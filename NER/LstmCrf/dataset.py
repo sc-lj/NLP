ec4a87d34c1068e8d79b6f4bcf2b47ce42b00270
lj <779365135@qq.com>
2018-11-01 23:23:10 +0800

    实体抽取

--- NER/LstmCrf/dataset.py
+++ NER/LstmCrf/dataset.py
@@ -1,10 +1,14 @@
 # coding:utf-8
 
 import tensorflow as tf
+from threading import Thread
+from queue import Queue
 
 class Dataset():
     def __init__(self,hps):
         self._hps=hps
+
+
 
 
 
---
+++ NER/LstmCrf/genVocab.py
@@ -0,0 +1,7 @@
+# coding:utf-8
+
+
+class Vocab():
+    def __init__(self):
+        self.files=''
+
