9d94232056ba2a4ae0e9ed1b33f18df304311af9
lujun <779365135@qq.com>
2018-11-12 10:58:13 +0800

    文本分类

--- Classify/TraMLearn/Data.py
+++ Classify/TraMLearn/Data.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(path))
 from KwordExtr.SKE.main import SemankeyWord
 from KwordExtr.TextRank.TextRankKeyword import TextRankKeyword
 from multiprocessing import Pool,cpu_count
+from random import shuffle
 
 def TRKeyWord(line,trkeyword):
     lable, content = line.split(maxsplit=1)
@@ -23,7 +24,7 @@ def genKeyWords(files):
             keyword=pool.apply_async(func=TRKeyWord,args=(line,trkeyword))
             words=keyword.get()
             data.append(words)
-
+    shuffle(data)
     newdata=[a[1] for a in data]
     labels=[a[0] for a in data]
     return newdata,labels
